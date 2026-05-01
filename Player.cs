abe84e162c86fc66652f881d9c69da07c59dcb16
Hecateo <juniorcubamontero83@gmail.com>
2026-04-30 22:14:25 -0500

    player
    playercommit1

---
+++ Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Tarea4
+{
+    internal class Player
+    {
+        public string playername;
+        public int lifePlayer=3;
+        public void run()
+        {
+            Console.WriteLine("Introdusca su nombre:");
+            playername = Console.ReadLine();
+
+            lifePlayer = 3;
+            Console.WriteLine($"Su nombre es {playername} y su cantidad de vida es {lifePlayer}");
+        }
+
+    }
+}
